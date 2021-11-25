db18ac5d8e20856ad15ec681da1d24bee09c2d68
Arsen <62416244+arssssen@users.noreply.github.com>
2021-11-26 03:45:28 +0600

    3.2

---
+++ js/jquery-script.js
@@ -0,0 +1,55 @@
+const userNameEmail = $("#userNameEmail");
+const password = $("#password");
+const userName = $("#userName");
+const phone = $("#phone");
+
+
+$("#signIn").submit(function(e){
+    e.preventDefault()
+    submitCheck()
+})
+function submitCheck() {
+	
+	if(userNameEmail.val() === '') {
+		invalid(userNameEmail, 'This field cannot be empty');
+	} else {
+		valid(userNameEmail, "Good!");
+	}
+    if(userName.val() === '') {
+		invalid(userName, 'This field cannot be empty');
+	} else {
+		valid(userName, "Good!");
+	}
+    if(phone.val() === '') {
+		invalid(phone, 'This field cannot be empty');
+	} else {
+		valid(phone, "Good!");
+	}
+	if(password.val() === '') {
+		invalid(password, 'This field cannot be empty');
+	} else {
+		valid(password, "Good!");
+	}
+	
+}
+function invalid(input, message) {
+    input.removeClass("is-valid").addClass("is-invalid")
+    input.next().text(message)
+}
+
+function valid(input, message) {
+    input.removeClass("is-invalid").addClass("is-valid")
+    input.next().text(message)
+}
+	
+
+
+
+
+
+
+
+
+
+
+
